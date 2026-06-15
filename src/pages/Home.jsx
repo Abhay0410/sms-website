cda9871c4bd3119464c57465eaea59960b7a6d3b
Abhay0410 <kumarsinghabhay77@gmail.com>
2026-06-15 14:28:29 +0530

    hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import FeatureWalkthrough from '../components/FeatureWalkthrough';
-import HeroSection from '../components/HeroSection';
 import Testimonials from '../components/Testimonials';
+import HeroImg from '../assets/eduzagerhomehero.png';
 
 const Home = () => (
   <div className="flex flex-col items-center w-full overflow-hidden">
-    <HeroSection />
+    {/* Full-width Hero Image */}
+    <section className="w-full bg-white">
+      <img
+        src={HeroImg}
+        alt="EduZager School Management Platform"
+        className="w-full h-auto"
+      />
+    </section>
     <FeatureWalkthrough />
     <Testimonials />
   </div>
